890ab5719f89b2319abb22ad790d108bb9d62c01
andreabizzoni <andrea.bizzoni@outlook.com>
2025-12-25 15:12:40 +0100

    fix datetime import

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -3,7 +3,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import logging
-import datetime
+from datetime import datetime
 from dotenv import load_dotenv
 
 from ..models.llm_response import EmailLLMResponse
